5001702b7f066e644598685d95c5695b4d39e25a
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-11-22 17:10:20 +0900

    feat: define lambda terms

--- main.go
+++ main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
+	"fmt"
+	//"context"
+	//"encoding/json"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	//"github.com/aws/aws-lambda-go/events"
+	//"github.com/aws/aws-lambda-go/lambda"
 )
 
 type Headers struct {}
-type Response events.APIGatewayProxyResponse
+//type Response events.APIGatewayProxyResponse
 type Request struct {
 	Src string `json:src`
 }
 
+type Name string
+
+type Var struct {
+	name string
+}
+
+type Lam struct {
+	name string
+	expr Expr
+}
+
+type App struct {
+	f Expr
+	arg Expr
+}
+
+type Expr interface {
+	reduce() Expr
+}
+
+func (x Var) reduce() Expr {
+	return x
+}
+
+func (x Lam) reduce() Expr {
+	return x
+}
+
+func (x App) reduce() Expr {
+	f := x.f.reduce()
+	arg := x.arg.reduce()
+	switch f.(type) {
+	case Lam:
+		return f
+	default:
+		return App{f, arg}
+	}
+}
+
+/*
 func Handler(ctx context.Context, gwreq events.APIGatewayProxyRequest) (Response, error) {
 	body := ([]byte)(gwreq.Body)
 	req := new(Request)
@@ -32,4 +73,10 @@ func Handler(ctx context.Context, gwreq events.APIGatewayProxyRequest) (Response
 
 func main() {
 	lambda.Start(Handler)
+}
+
+*/
+func main(){
+	x := App{Lam{"x", Var{"x"}}, Var{"y"}}
+	fmt.Println(x.reduce())
 }
